b9181b56646e68afc1bee359eea27f29e58e50d9
Afshin Mehrabani <afshin.meh@gmail.com>
2017-06-20 15:21:11 +0100

    using buffer to print the output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::env;
 
+fn create_buffer(size: usize) -> String {
+    String::with_capacity(size * 4096)
+}
+
 #[allow(while_true)]
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,15 +11,28 @@ fn main() {
     match args.len() {
         // no arguments passed
         1 => {
+            // 2 because of "y\n"
+            let mut s_buffer = create_buffer(2);
+
+            for _ in 0..4096 {
+                s_buffer.push_str("y\n");
+            }
+
             while true {
-                println!("{}", "y");
+                print!("{}", s_buffer);
             }
         },
         _ => {
             let s: String = args[1..].join("");
 
+            let mut s_buffer = create_buffer(s.len());
+
+            for _ in 0..4096 {
+                s_buffer.push_str(&s);
+            }
+
             while true {
-                println!("{}", s);
+                println!("{}", s_buffer);
             }
         }
     }
